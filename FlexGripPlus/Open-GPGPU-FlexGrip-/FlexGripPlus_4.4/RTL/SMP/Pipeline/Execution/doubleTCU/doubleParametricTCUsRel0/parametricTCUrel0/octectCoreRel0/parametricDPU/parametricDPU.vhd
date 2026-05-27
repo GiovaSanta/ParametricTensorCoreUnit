8fd77968a3bc46cf3c75d8d30e84f97afeb0eb9e
GiovaSanta <s308882@studenti.polito.it>
2026-05-27 12:44:26 +0200

    updated the parametric dpu related vhdl design a little (in flexgrip plus environment): Gate inactive FlexGrip parametric DPU inputs by selected format, this way simulation in modelsim happens faster then before

--- FlexGripPlus/Open-GPGPU-FlexGrip-/FlexGripPlus_4.4/RTL/SMP/Pipeline/Execution/doubleTCU/doubleParametricTCUsRel0/parametricTCUrel0/octectCoreRel0/parametricDPU/parametricDPU.vhd
+++ FlexGripPlus/Open-GPGPU-FlexGrip-/FlexGripPlus_4.4/RTL/SMP/Pipeline/Execution/doubleTCU/doubleParametricTCUsRel0/parametricTCUrel0/octectCoreRel0/parametricDPU/parametricDPU.vhd
@@ -268,38 +268,337 @@ component dot_unit_coreINT is
 	);
 end component;
 
+constant ZERO8  : std_logic_vector(7 downto 0)  := (others => '0');
+constant ZERO16 : std_logic_vector(15 downto 0) := (others => '0');
+constant ZERO32 : std_logic_vector(31 downto 0) := (others => '0');
+
+signal is_fp8       : std_logic;
+signal is_fp16      : std_logic;
+signal is_fp32      : std_logic;
+signal is_posit8    : std_logic;
+signal is_posit16   : std_logic;
+signal is_posit32   : std_logic;
+signal is_fixP8_16  : std_logic;
+signal is_fixP16_32 : std_logic;
+signal is_int8_16   : std_logic;
+signal is_int16_32  : std_logic;
+
+-- FP8 gated inputs
+signal A0_8_fp8_g, A1_8_fp8_g, A2_8_fp8_g, A3_8_fp8_g : std_logic_vector(7 downto 0);
+signal B0_8_fp8_g, B1_8_fp8_g, B2_8_fp8_g, B3_8_fp8_g : std_logic_vector(7 downto 0);
+signal C0_8_fp8_g : std_logic_vector(7 downto 0);
+
+-- FP16 gated inputs
+signal A0_16_fp16_g, A1_16_fp16_g, A2_16_fp16_g, A3_16_fp16_g : std_logic_vector(15 downto 0);
+signal B0_16_fp16_g, B1_16_fp16_g, B2_16_fp16_g, B3_16_fp16_g : std_logic_vector(15 downto 0);
+signal C0_16_fp16_g : std_logic_vector(15 downto 0);
+
+-- FP32 gated inputs
+signal A0_32_fp32_g, A1_32_fp32_g, A2_32_fp32_g, A3_32_fp32_g : std_logic_vector(31 downto 0);
+signal B0_32_fp32_g, B1_32_fp32_g, B2_32_fp32_g, B3_32_fp32_g : std_logic_vector(31 downto 0);
+signal C0_32_fp32_g : std_logic_vector(31 downto 0);
+
+-- POSIT8 gated inputs
+signal A0_8_posit8_g, A1_8_posit8_g, A2_8_posit8_g, A3_8_posit8_g : std_logic_vector(7 downto 0);
+signal B0_8_posit8_g, B1_8_posit8_g, B2_8_posit8_g, B3_8_posit8_g : std_logic_vector(7 downto 0);
+signal C0_8_posit8_g : std_logic_vector(7 downto 0);
+
+-- POSIT16 gated inputs
+signal A0_16_posit16_g, A1_16_posit16_g, A2_16_posit16_g, A3_16_posit16_g : std_logic_vector(15 downto 0);
+signal B0_16_posit16_g, B1_16_posit16_g, B2_16_posit16_g, B3_16_posit16_g : std_logic_vector(15 downto 0);
+signal C0_16_posit16_g : std_logic_vector(15 downto 0);
+
+-- POSIT32 gated inputs
+signal A0_32_posit32_g, A1_32_posit32_g, A2_32_posit32_g, A3_32_posit32_g : std_logic_vector(31 downto 0);
+signal B0_32_posit32_g, B1_32_posit32_g, B2_32_posit32_g, B3_32_posit32_g : std_logic_vector(31 downto 0);
+signal C0_32_posit32_g : std_logic_vector(31 downto 0);
+
+-- FixedP8_16 gated inputs
+signal A0_8_fixP8_16_g, A1_8_fixP8_16_g, A2_8_fixP8_16_g, A3_8_fixP8_16_g : std_logic_vector(7 downto 0);
+signal B0_8_fixP8_16_g, B1_8_fixP8_16_g, B2_8_fixP8_16_g, B3_8_fixP8_16_g : std_logic_vector(7 downto 0);
+signal C0_16_fixP8_16_g : std_logic_vector(15 downto 0);
+
+-- FixedP16_32 gated inputs
+signal A0_16_fixP16_32_g, A1_16_fixP16_32_g, A2_16_fixP16_32_g, A3_16_fixP16_32_g : std_logic_vector(15 downto 0);
+signal B0_16_fixP16_32_g, B1_16_fixP16_32_g, B2_16_fixP16_32_g, B3_16_fixP16_32_g : std_logic_vector(15 downto 0);
+signal C0_32_fixP16_32_g : std_logic_vector(31 downto 0);
+
+-- INT8_16 gated inputs
+signal A0_8_int8_16_g, A1_8_int8_16_g, A2_8_int8_16_g, A3_8_int8_16_g : std_logic_vector(7 downto 0);
+signal B0_8_int8_16_g, B1_8_int8_16_g, B2_8_int8_16_g, B3_8_int8_16_g : std_logic_vector(7 downto 0);
+signal C0_16_int8_16_g : std_logic_vector(15 downto 0);
+
+-- INT16_32 gated inputs
+signal A0_16_int16_32_g, A1_16_int16_32_g, A2_16_int16_32_g, A3_16_int16_32_g : std_logic_vector(15 downto 0);
+signal B0_16_int16_32_g, B1_16_int16_32_g, B2_16_int16_32_g, B3_16_int16_32_g : std_logic_vector(15 downto 0);
+signal C0_32_int16_32_g : std_logic_vector(31 downto 0);
+
 begin
 
+-- Format selection decoding
+is_fp8       <= '1' when widthSel = "00" and typeSel = "000" else '0';
+is_fp16      <= '1' when widthSel = "01" and typeSel = "000" else '0';
+is_fp32      <= '1' when widthSel = "10" and typeSel = "000" else '0';
+
+is_posit8    <= '1' when widthSel = "00" and typeSel = "001" else '0';
+is_posit16   <= '1' when widthSel = "01" and typeSel = "001" else '0';
+is_posit32   <= '1' when widthSel = "10" and typeSel = "001" else '0';
+
+is_fixP8_16  <= '1' when widthSel = "00" and typeSel = "010" else '0';
+is_fixP16_32 <= '1' when widthSel = "01" and typeSel = "010" else '0';
+
+is_int8_16   <= '1' when widthSel = "00" and typeSel = "011" else '0';
+is_int16_32  <= '1' when widthSel = "01" and typeSel = "011" else '0';
+
+-- FP8 gating
+A0_8_fp8_g <= A0_8 when is_fp8 = '1' else ZERO8;
+A1_8_fp8_g <= A1_8 when is_fp8 = '1' else ZERO8;
+A2_8_fp8_g <= A2_8 when is_fp8 = '1' else ZERO8;
+A3_8_fp8_g <= A3_8 when is_fp8 = '1' else ZERO8;
+B0_8_fp8_g <= B0_8 when is_fp8 = '1' else ZERO8;
+B1_8_fp8_g <= B1_8 when is_fp8 = '1' else ZERO8;
+B2_8_fp8_g <= B2_8 when is_fp8 = '1' else ZERO8;
+B3_8_fp8_g <= B3_8 when is_fp8 = '1' else ZERO8;
+C0_8_fp8_g <= C0_8 when is_fp8 = '1' else ZERO8;
+
+-- FP16 gating
+A0_16_fp16_g <= A0_16 when is_fp16 = '1' else ZERO16;
+A1_16_fp16_g <= A1_16 when is_fp16 = '1' else ZERO16;
+A2_16_fp16_g <= A2_16 when is_fp16 = '1' else ZERO16;
+A3_16_fp16_g <= A3_16 when is_fp16 = '1' else ZERO16;
+B0_16_fp16_g <= B0_16 when is_fp16 = '1' else ZERO16;
+B1_16_fp16_g <= B1_16 when is_fp16 = '1' else ZERO16;
+B2_16_fp16_g <= B2_16 when is_fp16 = '1' else ZERO16;
+B3_16_fp16_g <= B3_16 when is_fp16 = '1' else ZERO16;
+C0_16_fp16_g <= C0_16 when is_fp16 = '1' else ZERO16;
+
+-- FP32 gating
+A0_32_fp32_g <= A0_32 when is_fp32 = '1' else ZERO32;
+A1_32_fp32_g <= A1_32 when is_fp32 = '1' else ZERO32;
+A2_32_fp32_g <= A2_32 when is_fp32 = '1' else ZERO32;
+A3_32_fp32_g <= A3_32 when is_fp32 = '1' else ZERO32;
+B0_32_fp32_g <= B0_32 when is_fp32 = '1' else ZERO32;
+B1_32_fp32_g <= B1_32 when is_fp32 = '1' else ZERO32;
+B2_32_fp32_g <= B2_32 when is_fp32 = '1' else ZERO32;
+B3_32_fp32_g <= B3_32 when is_fp32 = '1' else ZERO32;
+C0_32_fp32_g <= C0_32 when is_fp32 = '1' else ZERO32;
+
+-- POSIT8 gating
+A0_8_posit8_g <= A0_8 when is_posit8 = '1' else ZERO8;
+A1_8_posit8_g <= A1_8 when is_posit8 = '1' else ZERO8;
+A2_8_posit8_g <= A2_8 when is_posit8 = '1' else ZERO8;
+A3_8_posit8_g <= A3_8 when is_posit8 = '1' else ZERO8;
+B0_8_posit8_g <= B0_8 when is_posit8 = '1' else ZERO8;
+B1_8_posit8_g <= B1_8 when is_posit8 = '1' else ZERO8;
+B2_8_posit8_g <= B2_8 when is_posit8 = '1' else ZERO8;
+B3_8_posit8_g <= B3_8 when is_posit8 = '1' else ZERO8;
+C0_8_posit8_g <= C0_8 when is_posit8 = '1' else ZERO8;
+
+-- POSIT16 gating
+A0_16_posit16_g <= A0_16 when is_posit16 = '1' else ZERO16;
+A1_16_posit16_g <= A1_16 when is_posit16 = '1' else ZERO16;
+A2_16_posit16_g <= A2_16 when is_posit16 = '1' else ZERO16;
+A3_16_posit16_g <= A3_16 when is_posit16 = '1' else ZERO16;
+B0_16_posit16_g <= B0_16 when is_posit16 = '1' else ZERO16;
+B1_16_posit16_g <= B1_16 when is_posit16 = '1' else ZERO16;
+B2_16_posit16_g <= B2_16 when is_posit16 = '1' else ZERO16;
+B3_16_posit16_g <= B3_16 when is_posit16 = '1' else ZERO16;
+C0_16_posit16_g <= C0_16 when is_posit16 = '1' else ZERO16;
+
+-- POSIT32 gating
+A0_32_posit32_g <= A0_32 when is_posit32 = '1' else ZERO32;
+A1_32_posit32_g <= A1_32 when is_posit32 = '1' else ZERO32;
+A2_32_posit32_g <= A2_32 when is_posit32 = '1' else ZERO32;
+A3_32_posit32_g <= A3_32 when is_posit32 = '1' else ZERO32;
+B0_32_posit32_g <= B0_32 when is_posit32 = '1' else ZERO32;
+B1_32_posit32_g <= B1_32 when is_posit32 = '1' else ZERO32;
+B2_32_posit32_g <= B2_32 when is_posit32 = '1' else ZERO32;
+B3_32_posit32_g <= B3_32 when is_posit32 = '1' else ZERO32;
+C0_32_posit32_g <= C0_32 when is_posit32 = '1' else ZERO32;
+
+-- FixedP8_16 gating
+A0_8_fixP8_16_g <= A0_8 when is_fixP8_16 = '1' else ZERO8;
+A1_8_fixP8_16_g <= A1_8 when is_fixP8_16 = '1' else ZERO8;
+A2_8_fixP8_16_g <= A2_8 when is_fixP8_16 = '1' else ZERO8;
+A3_8_fixP8_16_g <= A3_8 when is_fixP8_16 = '1' else ZERO8;
+B0_8_fixP8_16_g <= B0_8 when is_fixP8_16 = '1' else ZERO8;
+B1_8_fixP8_16_g <= B1_8 when is_fixP8_16 = '1' else ZERO8;
+B2_8_fixP8_16_g <= B2_8 when is_fixP8_16 = '1' else ZERO8;
+B3_8_fixP8_16_g <= B3_8 when is_fixP8_16 = '1' else ZERO8;
+C0_16_fixP8_16_g <= C0_16 when is_fixP8_16 = '1' else ZERO16;
+
+-- FixedP16_32 gating
+A0_16_fixP16_32_g <= A0_16 when is_fixP16_32 = '1' else ZERO16;
+A1_16_fixP16_32_g <= A1_16 when is_fixP16_32 = '1' else ZERO16;
+A2_16_fixP16_32_g <= A2_16 when is_fixP16_32 = '1' else ZERO16;
+A3_16_fixP16_32_g <= A3_16 when is_fixP16_32 = '1' else ZERO16;
+B0_16_fixP16_32_g <= B0_16 when is_fixP16_32 = '1' else ZERO16;
+B1_16_fixP16_32_g <= B1_16 when is_fixP16_32 = '1' else ZERO16;
+B2_16_fixP16_32_g <= B2_16 when is_fixP16_32 = '1' else ZERO16;
+B3_16_fixP16_32_g <= B3_16 when is_fixP16_32 = '1' else ZERO16;
+C0_32_fixP16_32_g <= C0_32 when is_fixP16_32 = '1' else ZERO32;
+
+-- INT8_16 gating
+A0_8_int8_16_g <= A0_8 when is_int8_16 = '1' else ZERO8;
+A1_8_int8_16_g <= A1_8 when is_int8_16 = '1' else ZERO8;
+A2_8_int8_16_g <= A2_8 when is_int8_16 = '1' else ZERO8;
+A3_8_int8_16_g <= A3_8 when is_int8_16 = '1' else ZERO8;
+B0_8_int8_16_g <= B0_8 when is_int8_16 = '1' else ZERO8;
+B1_8_int8_16_g <= B1_8 when is_int8_16 = '1' else ZERO8;
+B2_8_int8_16_g <= B2_8 when is_int8_16 = '1' else ZERO8;
+B3_8_int8_16_g <= B3_8 when is_int8_16 = '1' else ZERO8;
+C0_16_int8_16_g <= C0_16 when is_int8_16 = '1' else ZERO16;
+
+-- INT16_32 gating
+A0_16_int16_32_g <= A0_16 when is_int16_32 = '1' else ZERO16;
+A1_16_int16_32_g <= A1_16 when is_int16_32 = '1' else ZERO16;
+A2_16_int16_32_g <= A2_16 when is_int16_32 = '1' else ZERO16;
+A3_16_int16_32_g <= A3_16 when is_int16_32 = '1' else ZERO16;
+B0_16_int16_32_g <= B0_16 when is_int16_32 = '1' else ZERO16;
+B1_16_int16_32_g <= B1_16 when is_int16_32 = '1' else ZERO16;
+B2_16_int16_32_g <= B2_16 when is_int16_32 = '1' else ZERO16;
+B3_16_int16_32_g <= B3_16 when is_int16_32 = '1' else ZERO16;
+C0_32_int16_32_g <= C0_32 when is_int16_32 = '1' else ZERO32;
+
 dpu_fp8 : DotProductUnitFP8e4m3 
-    port map ( aX0 => A0_8, aX1 => A1_8, aX2 => A2_8, aX3 => A3_8, bY0 => B0_8, bY1 => B1_8, bY2 => B2_8 , bY3 => B3_8 , cX0 => C0_8 , R => out_DPU_FP8  ) ;
+    port map (
+        aX0 => A0_8_fp8_g,
+        aX1 => A1_8_fp8_g,
+        aX2 => A2_8_fp8_g,
+        aX3 => A3_8_fp8_g,
+        bY0 => B0_8_fp8_g,
+        bY1 => B1_8_fp8_g,
+        bY2 => B2_8_fp8_g,
+        bY3 => B3_8_fp8_g,
+        cX0 => C0_8_fp8_g,
+        R   => out_DPU_FP8
+    );
 
 dpu_fp16: DotProductUnitFP16
-   port map ( aX0 => A0_16, aX1 => A1_16, aX2 => A2_16, aX3 => A3_16, bY0 => B0_16, bY1 => B1_16, bY2 => B2_16 , bY3 => B3_16 , cX0 => C0_16 , R => out_DPU_FP16  ) ;
+    port map (
+        aX0 => A0_16_fp16_g,
+        aX1 => A1_16_fp16_g,
+        aX2 => A2_16_fp16_g,
+        aX3 => A3_16_fp16_g,
+        bY0 => B0_16_fp16_g,
+        bY1 => B1_16_fp16_g,
+        bY2 => B2_16_fp16_g,
+        bY3 => B3_16_fp16_g,
+        cX0 => C0_16_fp16_g,
+        R   => out_DPU_FP16
+    );
 
 dpu_fp32: DotProductUnitFP32
-    port map ( aX0 => A0_32, aX1 => A1_32, aX2 => A2_32, aX3 => A3_32, bY0 => B0_32, bY1 => B1_32, bY2 => B2_32 , bY3 => B3_32 , cX0 => C0_32 , R => out_DPU_FP32  ) ;
+    port map (
+        aX0 => A0_32_fp32_g,
+        aX1 => A1_32_fp32_g,
+        aX2 => A2_32_fp32_g,
+        aX3 => A3_32_fp32_g,
+        bY0 => B0_32_fp32_g,
+        bY1 => B1_32_fp32_g,
+        bY2 => B2_32_fp32_g,
+        bY3 => B3_32_fp32_g,
+        cX0 => C0_32_fp32_g,
+        R   => out_DPU_FP32
+    );
 
 dpu_posit8: DotProductUnitPosit
-    port map ( aX0 => A0_8 , aX1 => A1_8 , aX2 => A2_8 , aX3 => A3_8 , bY0 => B0_8 , bY1 => B1_8 , bY2 => B2_8, bY3 => B3_8 , cX0 => C0_8 , R => out_DPU_posit8  ) ;
+    port map (
+        aX0 => A0_8_posit8_g,
+        aX1 => A1_8_posit8_g,
+        aX2 => A2_8_posit8_g,
+        aX3 => A3_8_posit8_g,
+        bY0 => B0_8_posit8_g,
+        bY1 => B1_8_posit8_g,
+        bY2 => B2_8_posit8_g,
+        bY3 => B3_8_posit8_g,
+        cX0 => C0_8_posit8_g,
+        R   => out_DPU_posit8
+    );
 
 dpu_posit16: DotProductUnitPosit16
-    port map ( aX0 => A0_16 , aX1 => A1_16 , aX2 => A2_16 , aX3 => A3_16 , bY0 => B0_16 , bY1 => B1_16 , bY2 => B2_16, bY3 => B3_16 , cX0 => C0_16 , R => out_DPU_posit16 ) ;
+    port map (
+        aX0 => A0_16_posit16_g,
+        aX1 => A1_16_posit16_g,
+        aX2 => A2_16_posit16_g,
+        aX3 => A3_16_posit16_g,
+        bY0 => B0_16_posit16_g,
+        bY1 => B1_16_posit16_g,
+        bY2 => B2_16_posit16_g,
+        bY3 => B3_16_posit16_g,
+        cX0 => C0_16_posit16_g,
+        R   => out_DPU_posit16
+    );
 
 dpu_posit32: DotProductUnitPosit32
-    port map ( aX0 => A0_32 , aX1 => A1_32 , aX2 => A2_32 , aX3 => A3_32 , bY0 => B0_32 , bY1 => B1_32 , bY2 => B2_32, bY3 => B3_32 , cX0 => C0_32 , R => out_DPU_posit32 ) ;
+    port map (
+        aX0 => A0_32_posit32_g,
+        aX1 => A1_32_posit32_g,
+        aX2 => A2_32_posit32_g,
+        aX3 => A3_32_posit32_g,
+        bY0 => B0_32_posit32_g,
+        bY1 => B1_32_posit32_g,
+        bY2 => B2_32_posit32_g,
+        bY3 => B3_32_posit32_g,
+        cX0 => C0_32_posit32_g,
+        R   => out_DPU_posit32
+    );
 
 dpu_FixP8_16: DotProductUnit_FixedPoint8_16 
-    port map ( aX0 => A0_8 , aX1 => A1_8 , aX2 => A2_8 , aX3 => A3_8 , bY0 => B0_8 , bY1 => B1_8 , bY2 => B2_8, bY3 => B3_8 , cX0 => C0_16 , R => out_DPU_FixP8_16 ) ;
+    port map (
+        aX0 => A0_8_fixP8_16_g,
+        aX1 => A1_8_fixP8_16_g,
+        aX2 => A2_8_fixP8_16_g,
+        aX3 => A3_8_fixP8_16_g,
+        bY0 => B0_8_fixP8_16_g,
+        bY1 => B1_8_fixP8_16_g,
+        bY2 => B2_8_fixP8_16_g,
+        bY3 => B3_8_fixP8_16_g,
+        cX0 => C0_16_fixP8_16_g,
+        R   => out_DPU_FixP8_16
+    );
 
 dpu_FixP16_32: DotProductUnit
-    port map (  aX0 => A0_16 , aX1 => A1_16 , aX2 => A2_16 , aX3 => A3_16 , bY0 => B0_16 , bY1 => B1_16 , bY2 => B2_16, bY3 => B3_16 , cX0 => C0_32 , R => out_DPU_FixP16_32 ) ;
+    port map (
+        aX0 => A0_16_fixP16_32_g,
+        aX1 => A1_16_fixP16_32_g,
+        aX2 => A2_16_fixP16_32_g,
+        aX3 => A3_16_fixP16_32_g,
+        bY0 => B0_16_fixP16_32_g,
+        bY1 => B1_16_fixP16_32_g,
+        bY2 => B2_16_fixP16_32_g,
+        bY3 => B3_16_fixP16_32_g,
+        cX0 => C0_32_fixP16_32_g,
+        R   => out_DPU_FixP16_32
+    );
 
 dpu_int8_16: dot_unit_coreINT8
-    port map ( a_X0 => A0_8, a_X1 => A1_8, a_X2 => A2_8, a_X3 => A3_8, b_X0 => B0_8, b_X1 => B1_8, b_X2 => B2_8, b_X3 => B3_8, c_X0 => C0_16, w_XX3 => out_DPU_int8_16 ) ;
-    
-dpu_int16_32: dot_unit_coreINT
-    port map ( a_X0 => A0_16, a_X1 => A1_16, a_X2 => A2_16, a_X3 => A3_16, b_X0 => B0_16, b_X1 => B1_16, b_X2 => B2_16, b_X3 =>	B3_16, c_X0 => C0_32, w_XX3 => out_DPU_int16_32 ) ;
+    port map (
+        a_X0 => A0_8_int8_16_g,
+        a_X1 => A1_8_int8_16_g,
+        a_X2 => A2_8_int8_16_g,
+        a_X3 => A3_8_int8_16_g,
+        b_X0 => B0_8_int8_16_g,
+        b_X1 => B1_8_int8_16_g,
+        b_X2 => B2_8_int8_16_g,
+        b_X3 => B3_8_int8_16_g,
+        c_X0 => C0_16_int8_16_g,
+        w_XX3 => out_DPU_int8_16
+    );
 
+dpu_int16_32: dot_unit_coreINT
+    port map (
+        a_X0 => A0_16_int16_32_g,
+        a_X1 => A1_16_int16_32_g,
+        a_X2 => A2_16_int16_32_g,
+        a_X3 => A3_16_int16_32_g,
+        b_X0 => B0_16_int16_32_g,
+        b_X1 => B1_16_int16_32_g,
+        b_X2 => B2_16_int16_32_g,
+        b_X3 => B3_16_int16_32_g,
+        c_X0 => C0_32_int16_32_g,
+        w_XX3 => out_DPU_int16_32
+    );
 mux : mux_rel0 
     port map ( widthSel => widthSel,
                typeSel => typeSel ,
